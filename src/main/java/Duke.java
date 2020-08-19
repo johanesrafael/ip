d805336a5fa3df5d300115b3cdcbbfc534be861c
johanesrafael <rafliong95@gmail.com>
2020-08-20 00:55:56 +0800

    Level 3: Mark As Done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,5 @@
-import java.util.ArrayList;
+/* import java.util.ArrayList; */
 import java.util.Scanner;
-
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -13,28 +12,36 @@ public class Duke {
                 + "What can I do for you?\n";
         System.out.println(greet);
         Scanner echo = new Scanner(System.in);
-        ArrayList<String> toDoList = new ArrayList<String>(100);
+        //ArrayList<String> toDoList = new ArrayList<String>(100);
 
         String usrin = echo.nextLine();
 
         if(!usrin.equals("Bye")) {
-            toDoList.add(usrin);
+
+            //toDoList.add(usrin);
+//          Task t = new Task(usrin);
+
             System.out.println("   Added: " + usrin);
 
             while (!usrin.equals("Bye")) {
                 usrin = echo.nextLine();
 
                 if(usrin.equals("list")){
+                    System.out.println("Here are the tasks in your list:\n");
+/*
                     for (String num: toDoList)
                     {
                         int i = toDoList.indexOf(num) + 1;
                         System.out.println("   " + i + ". " + num);
                     }
-                }
+*/
 
+                }
+//                if(!usrin.equals(""));
                 if(!usrin.equals("list"))
                     System.out.println("   Added: " + usrin);
-                toDoList.add(usrin);
+                //toDoList.add(usrin);
+                //t = new Task(usrin);
             }
         }
 
