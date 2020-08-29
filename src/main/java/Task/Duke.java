68bff51483ee039bf4dd388dfda28201442c5b93
johanesrafael <rafliong95@gmail.com>
2020-08-30 07:02:06 +0800

    Level-4. ToDos, Events, Deadlines

--- src/main/java/Task/Duke.java
+++ src/main/java/Task/Duke.java
@@ -7,28 +7,37 @@ public class Duke {
     // create counter for tracking each inserted user input
     private static int listCounter = 0;
 
-    // to insert user input to the list
+    // insert user input to the list
     public static void insertToList(String userInput) {
         if(userInput.startsWith("todo")) {
+            // take the description
             String description = userInput.split("todo")[1];
-            // create temporary task for passing over the user input to the actual task
+            // create To-Do task for passing over the user input to the actual task array
             Todo task = new Todo(description);
             // assign task into actual task and increment listCounter
             tasks[listCounter++] = task;
         }
         else if(userInput.startsWith("deadline")){
+            // create array to store words split by "/by"
             String[] by = userInput.split("/by");
+            // take the description
             String description = by[0].split("deadline")[1];
+            // create deadline task to be passed over to the actual task array
             Deadline task = new Deadline(description,by[1]);
+            // assign task into actual task and increment listCounter
             tasks[listCounter++] = task;
         }
         else if(userInput.startsWith("event")){
+            // create array to store words split by "/at"
             String[] at = userInput.split("/at");
+            // take the description
             String description = at[0].split("event")[1];
+            // create event task to be passed over to the actual task array
             Deadline task = new Deadline(description,at[1]);
+            // assign task into actual task and increment listCounter
             tasks[listCounter++] = task;
         }
-
+        // print the newly added task
         if (listCounter < 2) {
             System.out.println("   Got it. I've added this task:\n\t"
                     + tasks[listCounter-1] + "\n   Now you have " + listCounter + " task in the list.\n");
@@ -38,7 +47,7 @@ public class Duke {
         }
     }
 
-    // to view tasks
+    // view tasks
     public static void getList(){
         System.out.println("   Here are the tasks in your list:\n");
         for(int i = 0; i < listCounter; i++){
