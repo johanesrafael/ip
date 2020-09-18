a31387586e5a92dd15c56f091f8aaf6f78dfb2ee
johanesrafael <rafliong95@gmail.com>
2020-09-19 06:15:12 +0800

    Refactor Storage class

--- src/main/java/Task/DukeHelper/Storage.java
+++ src/main/java/Task/DukeHelper/Storage.java
@@ -1,4 +1,119 @@
 package Task.DukeHelper;
 
+import Task.Exception.OtherException;
+import Task.TaskType.Deadline;
+import Task.TaskType.Event;
+import Task.TaskType.ToDo;
+
+import java.io.*;
+import java.nio.charset.*;
+import java.nio.file.*;
+import java.util.*;
+
 public class Storage {
+    public static void readFile() throws IOException, OtherException {
+        // get path
+        Path path2 = Paths.get(Constants.path, Constants.fileName);
+        // find file
+        File file = new File(Constants.path, Constants.fileName);
+        // check file and its directory
+        isFolderExisted(file);
+        // change the file into string
+        String fileData = Files.readString(path2, StandardCharsets.UTF_8);
+        // scan the file data
+        Scanner data = new Scanner(fileData);
+        // make read by line then insert to the corresponding task
+        while(data.hasNextLine()){
+            insertExistingFileDataToTasks(data.nextLine());
+        }
+        data.close();
+    }
+
+    private static void isFolderExisted(File file) throws IOException {
+        if (!file.exists() && !file.isDirectory()) {
+            // make new directory
+            File dir = new File(Constants.folder);
+            dir.mkdir();
+            // create the file
+            Parser.createFile();
+        }
+    }
+
+    private static void isTaskDone(String userDataSymbol){
+        if(userDataSymbol.equals("\u2713")){
+            Parser.tasks.get(Parser.tasks.size()-1).setDone();
+        }
+    }
+
+    private static void passToToDo(String userData){
+        // take out the description
+        String description = userData.split("]")[2];
+        // take the symbol
+        String isTaskDone = userData.substring(4,5);
+        // create To-Do task for passing over the user input to the actual task array
+        ToDo task = new ToDo(description);
+        // assign task into actual task and increment listCounter
+        Parser.tasks.add(task);
+        // check whether the task is done
+        isTaskDone(isTaskDone);
+    }
+
+    private static void passToDeadline(String userData){
+        // erase brackets
+        userData = userData.replace("(","");
+        userData = userData.replace(")","");
+        // take symbol
+        String isTaskDone = userData.substring(4,5);
+        // take indexes
+        int descriptionStartIndex = userData.indexOf(" ");
+        int descriptionEndIndex = userData.indexOf("by:");
+        String description = userData.substring(descriptionStartIndex, descriptionEndIndex);
+        // take the event time
+        String by = userData.substring(descriptionEndIndex + 3);
+        // create event task to be passed over to the actual task array
+        Deadline task = new Deadline(description, by);
+        // assign task into actual task and increment listCounter
+        Parser.tasks.add(task);
+        // check whether the task is done
+        isTaskDone(isTaskDone);
+    }
+
+    private static void passToEvent(String userData){
+        // erase brackets
+        userData = userData.replace("(","");
+        userData = userData.replace(")","");
+        // take the symbol
+        String isTaskDone = userData.substring(4,5);
+        // take indexes
+        int descriptionStartIndex = userData.indexOf(" ");
+        int descriptionEndIndex = userData.indexOf("at:");
+        String description = userData.substring(descriptionStartIndex, descriptionEndIndex);
+        // take the event time
+        String at = userData.substring(descriptionEndIndex + 3);
+        // create event task to be passed over to the actual task array
+        Event task = new Event(description, at);
+        // assign task into actual task and increment listCounter
+        Parser.tasks.add(task);
+        // check whether the task is done
+        isTaskDone(isTaskDone);
+    }
+
+    private static void insertExistingFileDataToTasks(String userData) throws OtherException {
+        // take task type
+        char taskType = userData.charAt(1);
+        // insert the task based on task type
+        switch (taskType){
+        case 'T':
+            passToToDo(userData);
+            break;
+        case 'D':
+            passToDeadline(userData);
+            break;
+        case 'E':
+            passToEvent(userData);
+            break;
+        default:
+            throw new OtherException();
+        }
+    }
 }
