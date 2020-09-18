fa15c781280053e67727fd3ae03bafb13a4cfc43
johanesrafael <rafliong95@gmail.com>
2020-09-19 06:15:22 +0800

    Refactor Ui class

--- src/main/java/Task/DukeHelper/Ui.java
+++ src/main/java/Task/DukeHelper/Ui.java
@@ -1,2 +1,24 @@
-package Task.DukeHelper;public class Ui {
+package Task.DukeHelper;
+
+public class Ui {
+    public static void createLogo() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+    }
+
+    public static void greet() {
+        String greet = "Hello! I'm Duke\n"
+                + "What can I do for you?\n";
+        // print greet
+        System.out.println(greet);
+    }
+
+    public static void createByeMessage() {
+        String exit = "\nBye. Hope to see you again soon!\n";
+        System.out.println("   " + exit);
+    }
 }
