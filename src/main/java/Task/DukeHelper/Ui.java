aac8a379acfafe89423149c5fc50ed32caeb5398
johanesrafael <rafliong95@gmail.com>
2020-09-19 08:53:58 +0800

    Add some line separator for Ui

--- src/main/java/Task/DukeHelper/Ui.java
+++ src/main/java/Task/DukeHelper/Ui.java
@@ -7,44 +7,61 @@ public class Ui {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo + System.lineSeparator());
+        System.out.println("Hello from\n" + logo);
+        lineSeparator();
     }
 
     public static void greet() {
         String greet = "Hello! I'm Duke\n"
                 + "What can I do for you?\n";
         // print greet
-        System.out.println(greet + System.lineSeparator());
+        System.out.println(greet);
+        lineSeparator();
     }
 
     public static void createByeMessage() {
         String exit = "\nBye. Hope to see you again soon!\n";
-        System.out.println("   " + exit + System.lineSeparator());
+        System.out.println("   " + exit);
+        lineSeparator();
     }
 
     static void viewEmptyToDoMessage(){
+        showLine();
         System.out.println("\n ☹ OOPS!!! The description of a todo cannot be empty.\n");
+        lineSeparator();
     }
 
     static void getAddedTask() {
+        showLine();
         System.out.println("   Got it. I've added this task:\n\t"
                 + TaskList.tasks.get(TaskList.tasks.size()-1) + "\n   Now you have "
                 + TaskList.tasks.size() + " tasks in the list.\n");
+        lineSeparator();
     }
 
     static void viewInvalidCommandMessage() {
+        showLine();
         System.out.println("\n ☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n");
+        lineSeparator();
     }
 
     static void getListHeader() {
+        showLine();
         System.out.println(System.lineSeparator() + "   Here are the tasks in your list:\n");
     }
 
     static void lineSeparator(){
+        showLine();
         System.out.println(System.lineSeparator());
     }
 
+    static void showLine(){
+        System.out.println("_____________________________________________________________________");
+    }
+
     static void noFileOrDirectoryMessage() {
+        showLine();
         System.out.println("No such file or directory. Creating new directory and new file.");
+        lineSeparator();
     }
 }
