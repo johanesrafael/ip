257b21e24a40110c118a56553bcd5afd6954adb3
johanesrafael <rafliong95@gmail.com>
2020-10-03 23:07:59 +0800

    Fixed path issues

--- src/main/java/Task/DukeHelper/Constants.java
+++ src/main/java/Task/DukeHelper/Constants.java
@@ -7,9 +7,9 @@ public class Constants {
 
     // introduce a constant for list size
     public static final int MAX_LIST_SIZE = 100;
-    // create relative path
-    public static final String folder = "\\data";
-    public static final String path ="D:" + folder;
+    // create dynamic path
+    public static final String folder = "data";
+    public static final String path ="./" + folder;
     // create file name variable
     public static final String fileName = "progress.txt";
 
